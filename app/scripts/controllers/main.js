008d05c8c23f906a2b9be7d7336417438cfdea03
edyonil <edyonil@gmail.com>
2014-08-08 23:58:51 -0300

    Primeiro Commit do Projeto OpenSource FaqCMS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name faqCmsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the faqCmsApp
+ */
+angular.module('faqCmsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
